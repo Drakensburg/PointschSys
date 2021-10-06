d120bca90f884e7ec7940d6ebd5127851aeaf224
NachoWaffle <kiaanbruwer.kb@gmail.com>
2021-10-06 16:21:42 +0200

    Added report system

--- NWU_Pointsch_System/NWU_Pointsch_System/frmReport.Designer.cs
+++ NWU_Pointsch_System/NWU_Pointsch_System/frmReport.Designer.cs
@@ -29,21 +29,320 @@ namespace NWU_Pointsch_System
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReport));
+            this.picbPurpleThing = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lstReport = new System.Windows.Forms.ListBox();
+            this.gbReportOptions = new System.Windows.Forms.GroupBox();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnFilter = new System.Windows.Forms.Button();
+            this.gbCampus = new System.Windows.Forms.GroupBox();
+            this.cmbCampus = new System.Windows.Forms.ComboBox();
+            this.gbSortTotals = new System.Windows.Forms.GroupBox();
+            this.rbDesc = new System.Windows.Forms.RadioButton();
+            this.rbAsc = new System.Windows.Forms.RadioButton();
+            this.gbActions = new System.Windows.Forms.GroupBox();
+            this.gbMonth = new System.Windows.Forms.GroupBox();
+            this.cmbMonth = new System.Windows.Forms.ComboBox();
+            this.rbBoth = new System.Windows.Forms.RadioButton();
+            this.rbDiscipline = new System.Windows.Forms.RadioButton();
+            this.rbInfraction = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.picbPurpleThing)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.gbReportOptions.SuspendLayout();
+            this.gbCampus.SuspendLayout();
+            this.gbSortTotals.SuspendLayout();
+            this.gbActions.SuspendLayout();
+            this.gbMonth.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // picbPurpleThing
+            // 
+            this.picbPurpleThing.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picbPurpleThing.BackgroundImage")));
+            this.picbPurpleThing.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picbPurpleThing.Location = new System.Drawing.Point(-232, -225);
+            this.picbPurpleThing.Margin = new System.Windows.Forms.Padding(2);
+            this.picbPurpleThing.Name = "picbPurpleThing";
+            this.picbPurpleThing.Size = new System.Drawing.Size(335, 1010);
+            this.picbPurpleThing.TabIndex = 13;
+            this.picbPurpleThing.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox3.Location = new System.Drawing.Point(695, -234);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(5, 1010);
+            this.pictureBox3.TabIndex = 19;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(703, -240);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(5, 1010);
+            this.pictureBox2.TabIndex = 18;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(711, -247);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(5, 1010);
+            this.pictureBox1.TabIndex = 17;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lstReport
+            // 
+            this.lstReport.FormattingEnabled = true;
+            this.lstReport.Location = new System.Drawing.Point(108, 12);
+            this.lstReport.Name = "lstReport";
+            this.lstReport.Size = new System.Drawing.Size(582, 277);
+            this.lstReport.TabIndex = 20;
+            // 
+            // gbReportOptions
+            // 
+            this.gbReportOptions.Controls.Add(this.rbBoth);
+            this.gbReportOptions.Controls.Add(this.btnBack);
+            this.gbReportOptions.Controls.Add(this.btnClear);
+            this.gbReportOptions.Controls.Add(this.btnFilter);
+            this.gbReportOptions.Controls.Add(this.gbCampus);
+            this.gbReportOptions.Controls.Add(this.gbSortTotals);
+            this.gbReportOptions.Controls.Add(this.gbActions);
+            this.gbReportOptions.Controls.Add(this.gbMonth);
+            this.gbReportOptions.Location = new System.Drawing.Point(108, 305);
+            this.gbReportOptions.Name = "gbReportOptions";
+            this.gbReportOptions.Size = new System.Drawing.Size(582, 238);
+            this.gbReportOptions.TabIndex = 21;
+            this.gbReportOptions.TabStop = false;
+            this.gbReportOptions.Text = "REPORT OPTIONS";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(386, 156);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(104, 31);
+            this.btnBack.TabIndex = 7;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(443, 113);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(104, 31);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnFilter
+            // 
+            this.btnFilter.Location = new System.Drawing.Point(329, 113);
+            this.btnFilter.Name = "btnFilter";
+            this.btnFilter.Size = new System.Drawing.Size(104, 31);
+            this.btnFilter.TabIndex = 5;
+            this.btnFilter.Text = "Filter";
+            this.btnFilter.UseVisualStyleBackColor = true;
+            // 
+            // gbCampus
+            // 
+            this.gbCampus.Controls.Add(this.cmbCampus);
+            this.gbCampus.Location = new System.Drawing.Point(29, 77);
+            this.gbCampus.Name = "gbCampus";
+            this.gbCampus.Size = new System.Drawing.Size(262, 54);
+            this.gbCampus.TabIndex = 4;
+            this.gbCampus.TabStop = false;
+            this.gbCampus.Text = "Filter by campus";
+            // 
+            // cmbCampus
+            // 
+            this.cmbCampus.FormattingEnabled = true;
+            this.cmbCampus.Items.AddRange(new object[] {
+            "Potchefstroom",
+            "Mafikeng",
+            "Vaal"});
+            this.cmbCampus.Location = new System.Drawing.Point(6, 19);
+            this.cmbCampus.Name = "cmbCampus";
+            this.cmbCampus.Size = new System.Drawing.Size(250, 21);
+            this.cmbCampus.TabIndex = 0;
+            // 
+            // gbSortTotals
+            // 
+            this.gbSortTotals.Controls.Add(this.rbDesc);
+            this.gbSortTotals.Controls.Add(this.rbAsc);
+            this.gbSortTotals.Location = new System.Drawing.Point(329, 19);
+            this.gbSortTotals.Name = "gbSortTotals";
+            this.gbSortTotals.Size = new System.Drawing.Size(218, 66);
+            this.gbSortTotals.TabIndex = 3;
+            this.gbSortTotals.TabStop = false;
+            this.gbSortTotals.Text = "Sort by totals";
+            // 
+            // rbDesc
+            // 
+            this.rbDesc.AutoSize = true;
+            this.rbDesc.Location = new System.Drawing.Point(6, 42);
+            this.rbDesc.Name = "rbDesc";
+            this.rbDesc.Size = new System.Drawing.Size(82, 17);
+            this.rbDesc.TabIndex = 1;
+            this.rbDesc.TabStop = true;
+            this.rbDesc.Text = "Descending";
+            this.rbDesc.UseVisualStyleBackColor = true;
+            // 
+            // rbAsc
+            // 
+            this.rbAsc.AutoSize = true;
+            this.rbAsc.Location = new System.Drawing.Point(6, 19);
+            this.rbAsc.Name = "rbAsc";
+            this.rbAsc.Size = new System.Drawing.Size(75, 17);
+            this.rbAsc.TabIndex = 0;
+            this.rbAsc.TabStop = true;
+            this.rbAsc.Text = "Ascending";
+            this.rbAsc.UseVisualStyleBackColor = true;
+            // 
+            // gbActions
+            // 
+            this.gbActions.Controls.Add(this.rbInfraction);
+            this.gbActions.Controls.Add(this.rbDiscipline);
+            this.gbActions.Location = new System.Drawing.Point(29, 137);
+            this.gbActions.Name = "gbActions";
+            this.gbActions.Size = new System.Drawing.Size(262, 80);
+            this.gbActions.TabIndex = 2;
+            this.gbActions.TabStop = false;
+            this.gbActions.Text = "Filter by action";
+            // 
+            // gbMonth
+            // 
+            this.gbMonth.Controls.Add(this.cmbMonth);
+            this.gbMonth.Location = new System.Drawing.Point(29, 19);
+            this.gbMonth.Name = "gbMonth";
+            this.gbMonth.Size = new System.Drawing.Size(262, 52);
+            this.gbMonth.TabIndex = 1;
+            this.gbMonth.TabStop = false;
+            this.gbMonth.Text = "Filter by month";
+            // 
+            // cmbMonth
+            // 
+            this.cmbMonth.FormattingEnabled = true;
+            this.cmbMonth.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.cmbMonth.Location = new System.Drawing.Point(6, 19);
+            this.cmbMonth.Name = "cmbMonth";
+            this.cmbMonth.Size = new System.Drawing.Size(250, 21);
+            this.cmbMonth.TabIndex = 0;
+            // 
+            // rbBoth
+            // 
+            this.rbBoth.AutoSize = true;
+            this.rbBoth.Location = new System.Drawing.Point(35, 156);
+            this.rbBoth.Name = "rbBoth";
+            this.rbBoth.Size = new System.Drawing.Size(47, 17);
+            this.rbBoth.TabIndex = 0;
+            this.rbBoth.TabStop = true;
+            this.rbBoth.Text = "Both";
+            this.rbBoth.UseVisualStyleBackColor = true;
+            // 
+            // rbDiscipline
+            // 
+            this.rbDiscipline.AutoSize = true;
+            this.rbDiscipline.Location = new System.Drawing.Point(6, 42);
+            this.rbDiscipline.Name = "rbDiscipline";
+            this.rbDiscipline.Size = new System.Drawing.Size(70, 17);
+            this.rbDiscipline.TabIndex = 1;
+            this.rbDiscipline.TabStop = true;
+            this.rbDiscipline.Text = "Discipline";
+            this.rbDiscipline.UseVisualStyleBackColor = true;
+            // 
+            // rbInfraction
+            // 
+            this.rbInfraction.AutoSize = true;
+            this.rbInfraction.Location = new System.Drawing.Point(6, 65);
+            this.rbInfraction.Name = "rbInfraction";
+            this.rbInfraction.Size = new System.Drawing.Size(69, 17);
+            this.rbInfraction.TabIndex = 2;
+            this.rbInfraction.TabStop = true;
+            this.rbInfraction.Text = "Infraction";
+            this.rbInfraction.UseVisualStyleBackColor = true;
             // 
             // frmReport
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1135, 1315);
+            this.ClientSize = new System.Drawing.Size(716, 554);
+            this.Controls.Add(this.gbReportOptions);
+            this.Controls.Add(this.lstReport);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.picbPurpleThing);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmReport";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmReport";
+            this.Load += new System.EventHandler(this.frmReport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picbPurpleThing)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.gbReportOptions.ResumeLayout(false);
+            this.gbReportOptions.PerformLayout();
+            this.gbCampus.ResumeLayout(false);
+            this.gbSortTotals.ResumeLayout(false);
+            this.gbSortTotals.PerformLayout();
+            this.gbActions.ResumeLayout(false);
+            this.gbActions.PerformLayout();
+            this.gbMonth.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picbPurpleThing;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ListBox lstReport;
+        private System.Windows.Forms.GroupBox gbReportOptions;
+        private System.Windows.Forms.GroupBox gbSortTotals;
+        private System.Windows.Forms.RadioButton rbDesc;
+        private System.Windows.Forms.RadioButton rbAsc;
+        private System.Windows.Forms.GroupBox gbActions;
+        private System.Windows.Forms.GroupBox gbMonth;
+        private System.Windows.Forms.ComboBox cmbMonth;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnFilter;
+        private System.Windows.Forms.GroupBox gbCampus;
+        private System.Windows.Forms.ComboBox cmbCampus;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.RadioButton rbBoth;
+        private System.Windows.Forms.RadioButton rbInfraction;
+        private System.Windows.Forms.RadioButton rbDiscipline;
     }
 }
--- NWU_Pointsch_System/NWU_Pointsch_System/frmReport.cs
+++ NWU_Pointsch_System/NWU_Pointsch_System/frmReport.cs
@@ -7,14 +7,115 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace NWU_Pointsch_System
 {
     public partial class frmReport : Form
     {
+        SqlConnection conn;
+        private string constr = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\dbPointsch.mdf;Integrated Security=True";
+        SqlDataReader reader;
+        SqlCommand comm;
+        private string sql;
+        private int total;
+
+
         public frmReport()
         {
             InitializeComponent();
+        }
+
+        private void frmReport_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new SqlConnection(constr);//Create a new sqlconnection instance
+                conn.Open();
+
+                FormatOutput();//Adding the appropriate columb headings to the listbox
+
+                /* Discipline table */
+                LoadDiscipline();
+
+                lstReport.Items.Add("------------------------------------------------------------------------------");
+
+                /* Infraction table */
+                LoadInfraction();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+
+        private void FormatOutput()
+        {
+            string header = "Pointsch Type\tPointsch Subtype\t\tTotal Previous Month\tTotal Present Month\n\n";
+
+            lstReport.Items.Add(header);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstReport.Items.Clear();
+            FormatOutput();
+            LoadDiscipline();
+            LoadInfraction();
+        }
+
+        private void LoadDiscipline()
+        {
+            sql = "SELECT Discipline_Type_Code, SUM(Discipline_Pointsch) AS Discipline_Total " +
+                  "FROM Discipline";
+
+            comm = new SqlCommand(sql, conn);
+            reader = comm.ExecuteReader();
+
+            total = 0;
+            while (reader.Read())
+            {
+                total += Convert.ToInt32(reader.GetValue(2));
+            }
+
+            lstReport.Items.Add("Discipline\t\t" + total);
+
+            while (reader.Read())
+            {
+                string output = "\t\t" + reader.GetValue(0) + "\t" + reader.GetValue(1);
+
+                lstReport.Items.Add(output);
+            }
+        }
+
+        private void LoadInfraction()
+        {
+            sql = "SELECT Infraction_Type_Code, SUM(Infraction_Pointsch) AS Infraction_Total " +
+                  "FROM Infraction";
+
+            comm = new SqlCommand(sql, conn);
+            reader = comm.ExecuteReader();
+
+            total = 0;
+            while (reader.Read())
+            {
+                total += Convert.ToInt32(reader.GetValue(2));
+            }
+
+            lstReport.Items.Add("Infraction\t\t" + total);
+
+            while (reader.Read())
+            {
+                string output = "\t\t" + reader.GetValue(0) + "\t" + reader.GetValue(1);
+
+                lstReport.Items.Add(output);
+            }
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
