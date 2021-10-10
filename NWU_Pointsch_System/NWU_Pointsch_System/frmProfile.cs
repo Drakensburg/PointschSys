43ad4a0ef527b6cc1c7c96e8eedc288c331d3af7
HugoBadenhorst <hugobadenhorst1@gmail.com>
2021-10-10 22:18:12 +0200

    Fixed bugs

--- NWU_Pointsch_System/NWU_Pointsch_System/frmProfile.cs
+++ NWU_Pointsch_System/NWU_Pointsch_System/frmProfile.cs
@@ -28,47 +28,43 @@ namespace NWU_Pointsch_System
 
         private void frmProfile_Load(object sender, EventArgs e)
         {
+  
             string sNum = lblSnoPhys.Text;
-            sql = "SELECT SUM(dt.Discipline_Pointsch) As Points, s.Student_ID, s.Student_Name " +
-                "FROM Student s LEFT JOIN Discipline d on d.Student_NWU_ID = s.Student_ID LEFT JOIN Discipline_Type dt on dt.Discipline_Type_Code = d.Discipline_Type_Code " +
-                "WHERE s.Student_ID = @StudentNumber GROUP BY s.Student_ID, s.Student_Name"; //get the acumilated Discipline pointsch
-
+            sql = "SELECT SUM(Discipline_Pointsch) AS POINTS FROM Discipline WHERE Student_NWU_ID = @StudentNumber"; //get the accumulate Discipline pointsch
 
             conn = new SqlConnection(conStr);
-            comm = new SqlCommand(sql);
-            comm.Parameters.AddWithValue("@StudentNumber", sNum);
             conn.Open();
+            comm = new SqlCommand(sql, conn);
+            comm.Parameters.AddWithValue("@StudentNumber", sNum);
             adap = new SqlDataAdapter(comm);
             ds = new DataSet();
             adap.Fill(ds);
             int SUM1 = Convert.ToInt32(ds.Tables[0].Rows[0][0].ToString());
             conn.Close();
 
-            lblDiscipline.Text = SUM1.ToString(); //Discipline total
+            lblPos.Text = SUM1.ToString(); //Discipline total
 
 
-            sql = "SELECT SUM(dt.Infraction_Pointsch) As Points, s.Student_ID, s.Student_Name " +
-                "FROM Student s LEFT JOIN Infraction d on d.Student_NWU_ID = s.Student_ID LEFT JOIN Infraction_Type dt on dt.Infraction_Type_Code = d.Infraction_Type_Code " +
-                "WHERE s.Student_ID = @StudentNumber GROUP BY s.Student_ID, s.Student_Name"; ////get the acumilated Infraction pointsch
+            sql = "SELECT SUM(Infraction_Pointsch) AS POINTS FROM Infraction WHERE Student_NWU_ID = @StudentNumber"; //get the accumulate Infraction pointsch
 
 
             conn = new SqlConnection(conStr);
-            comm = new SqlCommand(sql);
-            comm.Parameters.AddWithValue("@StudentNumber", sNum);
             conn.Open();
+            comm = new SqlCommand(sql, conn);
+            comm.Parameters.AddWithValue("@StudentNumber", sNum);
             adap = new SqlDataAdapter(comm);
             ds = new DataSet();
             adap.Fill(ds);
             int SUM2 = Convert.ToInt32(ds.Tables[0].Rows[0][0].ToString());
             conn.Close();
 
-            lblInfraction.Text = SUM2.ToString();//Infraction total
+            lblNeg.Text = SUM2.ToString();//Infraction total
             string path;
 
             if (SUM1 > SUM2)//Total pointsch
             {
-                lblTotal.Text = (SUM1 - SUM2).ToString();
-                lblTotal.ForeColor = Color.Purple;
+                lblSum.Text = (SUM1 - SUM2).ToString();
+                lblSum.ForeColor = Color.Purple;
                 if((SUM1 - SUM2) > 20)
                 {
                     try 
@@ -98,8 +94,8 @@ namespace NWU_Pointsch_System
             }
             else
             {
-                lblTotal.Text = (SUM2 - SUM1).ToString();
-                lblTotal.ForeColor = Color.DarkRed;
+                lblSum.Text = (SUM2 - SUM1).ToString();
+                lblSum.ForeColor = Color.DarkRed;
                 if ((SUM2 - SUM1) > 20)
                 {
                     try 
@@ -128,37 +124,47 @@ namespace NWU_Pointsch_System
                 }
             }
 
-            sql = "SELECT Infraction_Date, Infraction_Discription, Infraction_Pointsch FROM Infraction WHERE (Student_NWU_ID = @StudentNumber)"; //make a lis of Infractions
+            sql = "SELECT Infraction_Date, Infraction_Description, Infraction_Pointsch FROM Infraction WHERE (Student_NWU_ID = @StudentNumber)"; //make a lis of Infractions
             
             conn = new SqlConnection(conStr);
-            comm = new SqlCommand(sql);
-            comm.Parameters.AddWithValue("@StudentNumber", sNum);
             conn.Open();
+            comm = new SqlCommand(sql, conn);
+            comm.Parameters.AddWithValue("@StudentNumber", sNum);
             adap = new SqlDataAdapter(comm);
             reader = comm.ExecuteReader();
 
             while (reader.Read())
             {
-                string output = reader.GetValue(0) + "\n" + reader.GetValue(1) + "\n" + reader.GetValue(2)+ "\n\n";
-                lblInfraction.Text = (output);
+                string output = "Date: " + reader.GetValue(0);
+                lbInfraction.Items.Add(output);
+                output = "Description: " + reader.GetValue(1);
+                lbInfraction.Items.Add(output);
+                output = "Pointsch: " + reader.GetValue(2);
+                lbInfraction.Items.Add(output);
+                lbInfraction.Items.Add("\n");
             }
 
             conn.Close();
 
             
-            sql = "SELECT Discipline_Date, Discipline_Discription, Discipline_Pointsch FROM Discipline WHERE (Student_NWU_ID = @StudentNumber)"; //make a list of Discipline's
+            sql = "SELECT Discipline_Date, Discipline_Description, Discipline_Pointsch FROM Discipline WHERE (Student_NWU_ID = @StudentNumber)"; //make a list of Discipline's
 
             conn = new SqlConnection(conStr);
-            comm = new SqlCommand(sql);
-            comm.Parameters.AddWithValue("@StudentNumber", sNum);
             conn.Open();
+            comm = new SqlCommand(sql, conn);
+            comm.Parameters.AddWithValue("@StudentNumber", sNum);
             adap = new SqlDataAdapter(comm);
             reader = comm.ExecuteReader();
 
             while (reader.Read())
             {
-                string output = reader.GetValue(0) + "\n" + reader.GetValue(1) + "\n" + reader.GetValue(2) + "\n\n";
-                lblInfraction.Text = (output);
+                string output = "Date: " + reader.GetValue(0);
+                lbDiscipline.Items.Add(output);
+                output ="Description: " + reader.GetValue(1);
+                lbDiscipline.Items.Add(output);
+                output = "Pointsch: " + reader.GetValue(2);
+                lbDiscipline.Items.Add(output);
+                lbDiscipline.Items.Add("\n");
             }
 
             conn.Close();
@@ -176,11 +182,6 @@ namespace NWU_Pointsch_System
             {
                 //FORM IS NOT OPEN
             }
-        }
-
-        private void frmProfile_Load_1(object sender, EventArgs e)
-        {
-
         }
     }
 }
